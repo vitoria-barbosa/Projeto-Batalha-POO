8ebcff0061ebde95df984f3ca43a9a0b41991a4f
vitoria <vitoriabf2006@gmail.com>
2025-12-14 16:32:40 -0300

    :sparkles:feat: adiciona a Main - classe executável do projeto.

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import view.View;
+
+public class Main {
+    public static void main(String[] args) {
+        View view = new View();
+
+        view.iniciarJogo();
+        view.iniciarDuelos();
+        view.finalizarBatalha();
+    }
+}
